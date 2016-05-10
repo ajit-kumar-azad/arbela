fa58d0f38d4de5481272ee4492a03843fcdf1eea
Ajit Kumar <ajit.kumar@walkingtree.in>
2016-05-10 13:21:34 +0530

    Fixed the layout of timer card

--- app/view/cards/CurrentTime.js
+++ app/view/cards/CurrentTime.js
@@ -8,36 +8,50 @@ Ext.define('Arbela.view.cards.CurrentTime', {
 
     viewTemplate: {
         // header: false,
-        layout: {
-            type: 'vbox',
-            align: 'stretch'
-        },
+        // layout: {
+        //     type: 'hbox',
+        //     align: 'stretch'
+        // },
         title: 'Current Time',
         // height: 90,
         items: [{
+            xtype: 'container',
             cls: 'greenbg',
-            xtype: 'component',
+            layout: {
+                type: 'hbox',
+                align: 'stretch'
+            },
             viewModel: {
                 date: null,
                 time: null
             },
-            flex: 1,
-            bind: {
-                html: '<div><div style="float:left;width:30%;"><div style="text-align:center;font-size=24px;"><p>{textday}</p></div><div style="text-align:center;font-size:48px;"><p style="margin-top: 0px;padding-top: 10px;">{numday}</p></div></div><div style="float:left;padding-left:20px;padding-top:40px;" class="bigtext">{time}</div></div>'
-            },
-            afterRender: function() {
-                var myVar = setInterval(myTimer, 1000);
-                var me = this;
-
-                function myTimer() {
-                    var d = new Date();
-                    me.getViewModel().setData({
-                        textday : Ext.Date.format(d,  "l"),
-                        numday : Ext.Date.format(d,  "j"),
-                        time : d.toLocaleTimeString()
-                    });
+            items: [{
+                xtype: 'component',
+                width: 120,
+                bind: {
+                    html: '<div><div style="text-align:center;font-size=24px;"><p>{textday}</p></div><div style="text-align:center;font-size:48px;"><p style="margin-top: 0px;padding-top: 10px;">{numday}</p></div></div>'
                 }
-            }
+
+            },{
+                xtype: 'component',
+                flex: 1,
+                bind: {
+                    html: '<div style="padding-left:10px;padding-top:40px;" class="bigtext">{time}</div>'
+                },
+                afterRender: function() {
+                    var myVar = setInterval(myTimer, 1000);
+                    var me = this;
+
+                    function myTimer() {
+                        var d = new Date();
+                        me.up("container").getViewModel().setData({
+                            textday : Ext.Date.format(d,  "l"),
+                            numday : Ext.Date.format(d,  "j"),
+                            time : d.toLocaleTimeString()
+                        });
+                    }
+                }
+            }]
         }],
         tools: []
     }
